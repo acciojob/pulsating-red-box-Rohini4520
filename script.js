0c044932ef7eb3270b3ff8d57bc953d8ca6a6fd7
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2024-11-21 15:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+cy.get('.box', { timeout: 10000 }) // Increases timeout to 10 seconds
+  .should('have.css', 'width', '100px')
+  .should('have.css', 'height', '100px');
